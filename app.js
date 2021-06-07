df098260c6d70af83cc2fbce80bd9acd429ea2ae
Toni Iannucci <toni.iannucci@gmail.com>
2021-06-07 19:49:45 +0200

    hover Footer done

--- app.js
+++ app.js
@@ -1,6 +1,10 @@
 let boutonModale = document.getElementById('Suivant');
 let BoxModale = document.getElementsByClassName('modale');
 let boutonSuivant = document.getElementsByClassName('BoutonModale');
+let footerAccueil = document.getElementsByClassName('footerAccueil');
+let footerChapitreUn = document.getElementsByClassName('footerAccueil');
+let footerChapitreDeux = document.getElementsByClassName('footerAccueil');
+let footerChapitreTrois = document.getElementsByClassName('footerAccueil');
 
 
 function toggleClass(){
